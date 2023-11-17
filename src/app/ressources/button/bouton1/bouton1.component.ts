2ccc5cd2632b22d40fe8d1faf292ad1f01a450c0
Likeur <jacksimba28@gmail.com>
2023-11-17 23:58:36 +0200

    add one button to ressource components

---
+++ src/app/ressources/button/bouton1/bouton1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-bouton1',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './bouton1.component.html',
+  styles: ``
+})
+export class Bouton1Component {
+
+}
