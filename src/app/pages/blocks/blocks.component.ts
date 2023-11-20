4b462cc0a27beb5be39d4ce991800cd95fdfc779
Likeur <jacksimba28@gmail.com>
2023-11-20 03:17:53 +0200

    adding block component illustration

--- src/app/pages/blocks/blocks.component.ts
+++ src/app/pages/blocks/blocks.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-blocks',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, RouterModule],
   templateUrl: './blocks.component.html',
   styles: ``
 })
 export default class BlocksComponent {
 
+    blockElements = [
+      {
+        id : 1,
+        name : "Hero-section",
+        image : '../../../assets/images/hero.png',
+        route : '',
+        quantity: 3
+      },
+      {
+        id : 2,
+        name : "feature-section",
+        image : '../../../assets/images/futuresec.png',
+        route : '',
+        quantity: 2
+      },
+      {
+        id : 3,
+        name : "footer-section",
+        image : '../../../assets/images/footersec.png',
+        route : '',
+        quantity: 5
+      }
+    ]
 }
