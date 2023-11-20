609ecfaa7a8417c435dfe5f482ce6440374863a0
Likeur <jacksimba28@gmail.com>
2023-11-20 02:25:26 +0200

    navbar responsive toggle button add

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,6 +9,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+  logoPath = '../../../assets/images/ng ui new logo.png'
   navLinks = [
     {
       id: 1,
@@ -30,5 +31,36 @@ export class NavbarComponent {
       title: 'Templates',
       route: '/templates'
     },
-]
+  ]
+
+  isDarkmode(){
+    function darkmode(){
+     
+      const darkMode = document.querySelector('#darkmode')!
+      const darkModeIcon = document.querySelector('#darkmodeicon')!
+
+      document.documentElement.classList.toggle('dark')
+      darkMode.classList.toggle('rotate-45')
+      if (document.documentElement.className == 'dark'){
+          darkModeIcon.className = 'bx bx-sun'
+      } else{
+          darkModeIcon.className = 'bx bxs-moon'
+      }
+    }
+
+    darkmode()
+  }
+
+  NavToggle(){
+    const  navBox = document.querySelector('#navbox')!
+    const barUn = document.querySelector('.bar-un')!
+    const barDeux = document.querySelector('.bar-deux')!
+
+    navBox.classList.toggle('scale-0')
+    barUn.classList.toggle('rotate-45')
+    barUn.classList.toggle('mt-2')
+    barUn.classList.toggle('absolute')
+
+    barDeux.classList.toggle('-rotate-45')
+  }
 }
