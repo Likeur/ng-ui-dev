9d1b062f4241bf67fe5fb5081e7cdfba2da572a6
Likeur <jacksimba28@gmail.com>
2023-11-28 18:21:11 +0200

    working on landing page

--- src/app/pages/landingpage/landingpage.component.ts
+++ src/app/pages/landingpage/landingpage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, RouterModule],
+  imports: [CommonModule, NavbarComponent, RouterModule, FooterComponent],
   templateUrl: './landingpage.component.html',
 })
 export default class LandingpageComponent {
