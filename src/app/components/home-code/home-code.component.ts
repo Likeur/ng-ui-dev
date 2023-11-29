4a451a78fa2193ff314509ba202933575e62df16
Likeur <jacksimba28@gmail.com>
2023-11-29 19:00:51 +0200

    echec to add code snippet with prismaJS

--- src/app/components/home-code/home-code.component.ts
+++ src/app/components/home-code/home-code.component.ts
@@ -12,19 +12,7 @@ import { HighlightJsModule } from 'ngx-highlight-js';
 export class HomeCodeComponent {
 
   snippet = `
-        import { Component } from '@angular/core';
-        import { CommonModule } from '@angular/common';
-        import { RouterModule } from '@angular/router';
         
-        @Component({
-          selector: 'app-footer',
-          standalone: true,
-          imports: [CommonModule, RouterModule],
-          templateUrl: './footer.component.html',
-        })
-        export class FooterComponent {
-        
-        }
   
   `
 
