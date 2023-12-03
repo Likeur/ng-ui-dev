c0a1ac10e086c40e6d034e849cae08b64d9b7cee
Likeur <jacksimba28@gmail.com>
2023-12-03 23:26:28 +0200

    working on herosec1

--- src/app/ressources/allressources/allressources.component.ts
+++ src/app/ressources/allressources/allressources.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Herosec1Component } from '../herosec/herosec1/herosec1.component';
 
 @Component({
   selector: 'app-allressources',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Herosec1Component],
   templateUrl: './allressources.component.html',
 })
 export default class AllressourcesComponent {
