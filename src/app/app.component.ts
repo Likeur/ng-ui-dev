ff70cda6833f4afe58c41bae8400770c08c3002a
Likeur <jacksimba28@gmail.com>
2023-12-15 10:37:35 +0200

    refactoring on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styles: [],
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
-  title = 'ng-ui';
 }
