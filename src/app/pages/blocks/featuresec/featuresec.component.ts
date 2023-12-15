9f297589a10fd98efe6fc56f071236b4139eb650
Likeur <jacksimba28@gmail.com>
2023-12-15 10:31:07 +0200

    adding featuresecressource

--- src/app/pages/blocks/featuresec/featuresec.component.ts
+++ src/app/pages/blocks/featuresec/featuresec.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../../components/footer/footer.component';
+import { Featuresec1Component } from '../../../ressources/featureSec/featuresec1/featuresec1.component';
 
 @Component({
   selector: 'app-featuresec',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent, Featuresec1Component],
   templateUrl: './featuresec.component.html',
-  styles: ``
 })
 export default class FeaturesecComponent {
+  isDarkmode(){
+    function darkmode(){
+     
+      const darkMode = document.querySelector('#darkmode')!
+      const darkModeIcon = document.querySelector('#darkmodeicon')!
 
+      document.documentElement.classList.toggle('dark')
+      darkMode.classList.toggle('rotate-45')
+      if (document.documentElement.className == 'dark'){
+          darkModeIcon.className = 'bx bx-sun'
+      } else{
+          darkModeIcon.className = 'bx bxs-moon'
+      }
+    }
+
+    darkmode()
+  }
+
+  screenSize = 'w-full'
+
+  desktopView(){
+    this.screenSize = 'w-full'
+  }
+  tabletView(){
+    this.screenSize = 'w-[728px]'
+  }
+  mobileView(){
+    this.screenSize = 'w-[524px]'
+  }
 }
---
+++ src/app/ressources/featureSec/featuresec1/featuresec1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-featuresec1',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './featuresec1.component.html',
+  styles: ``
+})
+export class Featuresec1Component {
+
+}
